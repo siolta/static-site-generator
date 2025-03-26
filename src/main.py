c8a892f76a10b21a3c488fbcd39a9ee22b797bb3
Skylar Iolta <23390416+siolta@users.noreply.github.com>
2025-03-26 14:37:08 -0700

    add text node to html func

--- src/main.py
+++ src/main.py
@@ -3,48 +3,11 @@ from htmlnode import HTMLNode, LeafNode, ParentNode
 
 
 def main():
-    text_node = TextNode("some test text", TextType.LINK, "https://test.link")
-    print(text_node)
-    html_child_node = HTMLNode("p", "this is a paragraph")
-    html_node = HTMLNode(
-        "h1", "this is a header value", [html_child_node], {"target": "_blank"}
-    )
-    leaf_node = LeafNode("p", "this is a paragraph value", {"target": "leaf_node"})
-    print(html_node)
-    print(html_node.props_to_html())
-    print(leaf_node)
-    print(leaf_node.to_html())
-    leaf_node_link = LeafNode("a", "Click here!", {"href": "https://test.link"})
-    print(leaf_node_link.to_html())
-    leaf_node_div = LeafNode("div", "this is a div value")
-    print(leaf_node_div.to_html())
-    leaf_node_none = LeafNode(None, "this is a div value")
-    print(leaf_node_none.to_html())
-    node = ParentNode(
-        "p1",
-        [
-            LeafNode("b", "Bold text"),
-            LeafNode(None, "Normal text"),
-            ParentNode(
-                "p2",
-                [
-                    LeafNode("div2", "div2 text"),
-                    LeafNode(None, "Normal2 text"),
-                    ParentNode(
-                        "p3",
-                        [
-                            LeafNode("div3", "div3 text"),
-                            LeafNode(None, "Normal3 text"),
-                        ],
-                    ),
-                ],
-            ),
-            LeafNode(None, "Normal text"),
-        ],
-        {},
-    )
+    node = TextNode("this is some text", TextType.IMAGE, "https://test.link")
+    print(node)
+    html_node = text_node_to_html_node(node)
 
-    print(node.to_html())
+    print(html_node)
 
 
 if __name__ == "__main__":
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -34,6 +34,53 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(
             "TextNode(This is a text node, text, https://test.url)", repr(node)
         )
+
+
+class TestTextNodeToHTML(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_bold(self):
+        node = TextNode("This is bold", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is bold")
+
+    def test_italic(self):
+        node = TextNode("This is italic", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "This is italic")
+
+    def test_code(self):
+        node = TextNode("This is code", TextType.CODE)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "code")
+        self.assertEqual(html_node.value, "This is code")
+
+    def test_link(self):
+        node = TextNode("This is a link", TextType.LINK, "https://test.link")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.props, {"href": "https://test.link"})
+        self.assertEqual(html_node.value, "This is a link")
+
+    def test_image(self):
+        node = TextNode("This is a image node", TextType.IMAGE, "https://test.link")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(
+            html_node.props, {"src": "https://test.link", "alt": "This is a image node"}
+        )
+        self.assertEqual(html_node.value, "")
+
+    def test_error_case(self):
+        node = TextNode("", "BADTYPE")
+        with self.assertRaises(ValueError):
+            text_node_to_html_node(node)
 
 
 if __name__ == "__main__":
