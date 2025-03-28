415213e7fc454f6fec87fe62444d9d4de686fb02
Skylar Iolta <23390416+siolta@users.noreply.github.com>
2025-03-27 20:42:09 -0700

    initial node splitter

--- src/main.py
+++ src/main.py
@@ -1,13 +1,14 @@
 from textnode import TextNode, TextType
 from htmlnode import HTMLNode, LeafNode, ParentNode
+from node_helpers import split_nodes_delimiter
 
 
 def main():
-    node = TextNode("this is some text", TextType.IMAGE, "https://test.link")
-    print(node)
-    html_node = text_node_to_html_node(node)
-
-    print(html_node)
+    node1 = TextNode("This is text with `code block` words", TextType.TEXT)
+    node2 = TextNode("This is second text with `code block` words", TextType.TEXT)
+    node3 = TextNode("**code block**", TextType.BOLD)
+    new_nodes = split_nodes_delimiter([node1, node2, node3], "`", TextType.CODE)
+    print(new_nodes)
 
 
 if __name__ == "__main__":
