7c8792d8b99ba508534eecf3254f7dac9a6c730d
Skylar Iolta <23390416+siolta@users.noreply.github.com>
2025-03-28 00:18:38 -0700

    finish markdown line parsing and tests

--- src/main.py
+++ src/main.py
@@ -1,14 +1,10 @@
 from textnode import TextNode, TextType
 from htmlnode import HTMLNode, LeafNode, ParentNode
-from node_helpers import split_nodes_delimiter
+from inline_markdown import text_to_textnodes
 
 
 def main():
-    node1 = TextNode("This is text with `code block` words", TextType.TEXT)
-    node2 = TextNode("This is second text with `code block` words", TextType.TEXT)
-    node3 = TextNode("**code block**", TextType.BOLD)
-    new_nodes = split_nodes_delimiter([node1, node2, node3], "`", TextType.CODE)
-    print(new_nodes)
+    pass
 
 
 if __name__ == "__main__":
