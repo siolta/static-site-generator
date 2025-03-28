6c1ce06f27484613cb0b46436d31aff97f7198b4
Skylar Iolta <23390416+siolta@users.noreply.github.com>
2025-03-28 11:01:21 -0700

    add block enum

--- markdown_blocks.py
+++ markdown_blocks.py
@@ -1,3 +1,15 @@
+from enum import Enum
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+
 def markdown_to_blocks(document):
     blocks = [block.strip() for block in document.split("\n\n") if block != ""]
     return blocks
