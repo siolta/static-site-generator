5ba423808468a66c21ce0e17190e06cc95566a24
Skylar Iolta <23390416+siolta@users.noreply.github.com>
2025-04-01 23:32:57 -0700

    add block parsing

--- src/main.py
+++ src/main.py
@@ -1,31 +1,10 @@
 from textnode import TextNode, TextType
 from htmlnode import HTMLNode, LeafNode, ParentNode
-from markdown_blocks import block_to_block_type
-from markdown_to_html import markdown_to_html_node
+from markdown_blocks import block_to_block_type, markdown_to_html_node
 
 
 def main():
-    md = """
-##### HEADING
-
-This is **bolded** paragraph
-text in a p
-tag here
-
-This is another paragraph with _italic_ text and `code` here
-
-"""
-
-    md_code = """
-```
-This is text that _should_ remain 
-the **same** even with inline stuff
-```
-"""
-
-    node = markdown_to_html_node(md)
-    html = node.to_html()
-    print(html)
+    pass
 
 
 if __name__ == "__main__":
--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,6 +1,10 @@
 from enum import Enum
 import re
 
+from htmlnode import ParentNode
+from inline_markdown import text_to_textnodes
+from textnode import text_node_to_html_node, TextNode, TextType
+
 
 class BlockType(Enum):
     PARAGRAPH = "paragraph"
@@ -41,3 +45,98 @@ def block_to_block_type(block):
             i += 1
         return BlockType.OLIST
     return BlockType.PARAGRAPH
+
+
+def text_to_children(text):
+    children = []
+    for node in text_to_textnodes(text):
+        text_node = text_node_to_html_node(node)
+        children.append(text_node)
+    return children
+
+
+def markdown_to_html_node(md):
+    blocks = markdown_to_blocks(md)
+    children = []
+    for block in blocks:
+        html_node = block_to_html_node(block)
+        children.append(html_node)
+
+    return ParentNode("div", children, None)
+
+
+def block_to_html_node(block):
+    match block_to_block_type(block):
+        case BlockType.PARAGRAPH:
+            return paragraph_to_html_node(block)
+        case BlockType.HEADING:
+            return heading_to_html_node(block)
+        case BlockType.CODE:
+            return code_to_html_node(block)
+        case BlockType.OLIST:
+            return olist_to_html_node(block)
+        case BlockType.ULIST:
+            return ulist_to_html_node(block)
+        case BlockType.QUOTE:
+            return quote_to_html_node(block)
+        case _:
+            raise ValueError("invalid block type")
+
+
+def paragraph_to_html_node(block):
+    lines = block.split("\n")
+    paragraph = " ".join(lines)
+    children = text_to_children(paragraph)
+    return ParentNode("p", children)
+
+
+def heading_to_html_node(block):
+    match = re.match(r"^#{1,6}", block)
+    level = len(match.group(0))
+
+    if level + 1 >= len(block):
+        raise ValueError(f"invalid heading level: {level}")
+    children = text_to_children(block[level + 1:])
+    return ParentNode(f"h{level}", children)
+
+
+def code_to_html_node(block):
+    if not block.startswith("```") or not block.endswith("```"):
+        raise ValueError("invalid code block")
+    text = block[4:-3]
+    raw_text_node = TextNode(text, TextType.TEXT)
+    child = text_node_to_html_node(raw_text_node)
+    children = ParentNode("code", [child])
+    return ParentNode("pre", [children])
+
+
+def olist_to_html_node(block):
+    items = block.split("\n")
+    html_items = []
+    for item in items:
+        text = item[3:]
+        children = text_to_children(text)
+        html_items.append(ParentNode("li", children))
+    return ParentNode("ol", html_items)
+
+
+def ulist_to_html_node(block):
+    items = block.split("\n")
+    html_items = []
+    for item in items:
+        text = item[2:]
+        children = text_to_children(text)
+        html_items.append(ParentNode("li", children))
+    return ParentNode("ul", html_items)
+
+
+def quote_to_html_node(block):
+    lines = block.split("\n")
+    new_lines = []
+    for line in lines:
+        if not line.startswith(">"):
+            raise ValueError("invalid quote block")
+        new_lines.append(line.lstrip(">").strip())
+    content = " ".join(new_lines)
+    children = text_to_children(content)
+    return ParentNode("blockquote", children)
