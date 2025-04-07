807aba50fe173803ab98195f3850aec0373d8d0d
Skylar Iolta <23390416+siolta@users.noreply.github.com>
2025-04-06 21:57:20 -0700

    add flow

--- src/generate_page.py
+++ src/generate_page.py
@@ -1,2 +1,10 @@
 def extract_title(markdown):
+    # Split lines
+    # search each line for one that starts with "# "
+    # assume it can only be the first line? no, it could be after an image, link, etc
+    # if none exist, raise exception
+    pass
+
+
+def generate_page(from_path, template_path, dest_path):
     pass
