7fc3dfd7bbc782bb9f96c0678cc0df951fc327cf
Skylar Iolta <23390416+siolta@users.noreply.github.com>
2025-04-07 22:56:32 -0700

    add some initial logic

--- src/generate_page.py
+++ src/generate_page.py
@@ -3,7 +3,13 @@ def extract_title(markdown):
     # search each line for one that starts with "# "
     # assume it can only be the first line? no, it could be after an image, link, etc
     # if none exist, raise exception
-    pass
+    lines = markdown.split("\n")
+    print(markdown)
+    for line in lines:
+        if line[:2] == "# ":
+            return line.lstrip("# ").strip()
+
+        raise ValueError("no title found")
 
 
 def generate_page(from_path, template_path, dest_path):
--- src/main.py
+++ src/main.py
@@ -1,13 +1,21 @@
 from copystatic import clean_target_dir, copy_static_assets
+from generate_page import extract_title
 
 
 PUBLIC_DIR = "./public"
 STATIC_DIR = "./static"
 
+TEST_MD = """
+# this is a title
+
+## this is not
+"""
+
 
 def main():
-    clean_target_dir(PUBLIC_DIR)
-    copy_static_assets(STATIC_DIR, PUBLIC_DIR)
+    # clean_target_dir(PUBLIC_DIR)
+    # copy_static_assets(STATIC_DIR, PUBLIC_DIR)
+    print(extract_title(TEST_MD))
 
 
 if __name__ == "__main__":
